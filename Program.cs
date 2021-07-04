dba3d3a12ddf8b48a765213bcc9731b64b474c5e
stasbaranovich <119stas911@gmail.com>
2021-07-04 11:24:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           
+            string number = Console.ReadLine();
+            int numb = int.Parse(number);
+            int sum = 0;
+            int[] arr_numb = new int[number.Length];
+            for (int i = 0; i < arr_numb.Length; i++)
+            {
+                arr_numb[i] = numb % 10;
+                numb = numb / 10;
+                sum += arr_numb[i];
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
